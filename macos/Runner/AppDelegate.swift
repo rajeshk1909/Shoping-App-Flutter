21325414bfed03cc2ab4d9b42a913caa5fd52f92
rajeshk1909 <[rajeshkumark1909@gmail.com]>
2024-07-25 09:37:31 +0530

    IOS firebase Setup

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -1,8 +1,10 @@
 import Cocoa
 import FlutterMacOS
+import FirebaseCore
 
 @NSApplicationMain
 class AppDelegate: FlutterAppDelegate {
+  FirebaseApp.configure()
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
     return true
   }
